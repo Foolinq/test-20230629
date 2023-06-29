9fe0081ebeca2c0792e21112011a8da6cd6f4627
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-06-29 12:45:46 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -2,3 +2,20 @@ import streamlit as st
 
 st.write("""
 # Test(icles)""")
+
+input = st.text_area("Ouin", "Les gars sont on?", height=70)
+
+
+st.write("""
+***
+""")
+
+st.header("C on?")
+input
+
+if input == "Oui":
+  st.header("Ketttt")
+elif input == "Non":
+  st.header("Yo t off")
+else:
+  st.header("Comme si que...")
