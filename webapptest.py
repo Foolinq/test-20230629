15fa506ae20587908611108195eb3ab6955463ee
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-07-04 14:12:57 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -1,18 +1,41 @@
 import streamlit as st
 
-st.write("""
-# Les bhy sont on?""")
+def main():
+    st.title('Ovarian Cancer Diagnosis Interface')
 
-input = st.text_area("","",height=10)
+    st.write('Please enter the patient information below:')
 
+    # Text input for patient's name
+    patient_name = st.text_input('Patient Name')
 
-st.write("""
-***
-""")
+    # Slider for patient's age
+    patient_age = st.slider('Patient Age', 0, 100, 50)
 
-if input == "Oui":
-  st.header("Ketttt")
-elif input == "Non":
-  st.header("Yo t off")
-else:
-  st.header("Comme si que...")
+    # Slider for patient's BMI
+    patient_bmi = st.slider('Patient BMI', 10.0, 50.0, 25.0)
+
+    # Text input for self-described state of health
+    patient_health = st.text_input('Self-described State of Health')
+
+    # File uploader for patient's DNA sequence
+    dna_file = st.file_uploader('Upload DNA Sequence', type=['txt', 'fasta'])
+
+    if st.button('Submit'):
+        st.write('Patient Information:')
+        st.write('Name: ', patient_name)
+        st.write('Age: ', patient_age)
+        st.write('BMI: ', patient_bmi)
+        st.write('Self-described State of Health: ', patient_health)
+
+        if dna_file is not None:
+            # Read the file and store the DNA sequence
+            dna_sequence = dna_file.getvalue().decode()
+            st.write('DNA Sequence: ', dna_sequence)
+
+            # Here you can add the code to process the patient's information
+            # For example, you could call a function that uses a machine learning model to predict the likelihood of ovarian cancer based on the input data
+            # prediction = predict_cancer(patient_age, patient_bmi, patient_health, dna_sequence)
+            # st.write('Cancer Prediction: ', prediction)
+
+if __name__ == "__main__":
+    main()
