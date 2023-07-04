827967db036d7a844bf45cdc212cfea4c14c298c
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-07-04 14:58:17 -0400

    Update llm_processing.py

--- llm_processing.py
+++ llm_processing.py
@@ -4,7 +4,7 @@ from langchain import LLMChain, OpenAI, PromptTemplate
 def process_llm_request(doctor_question, patient_name, patient_age, patient_bmi, patient_health, symptoms):
     api_key = os.getenv('OPENAI_API_KEY')
     llm_request = f"{doctor_question} The patient's name is {patient_name}, age is {patient_age}, BMI is {patient_bmi}, and self-described state of health is {patient_health}. The reported symptoms are {symptoms}."
-    llm = LLMChain(llm=OpenAI(api_key=api_key), prompt=PromptTemplate(template=llm_request))
+    llm = LLMChain(llm=OpenAI(api_key=api_key), prompt=PromptTemplate(input_variables=["dummy"], template=llm_request))
     # Here you would call the function to process the request with the LLM model
     # result = llm.process_request()
     # return result
