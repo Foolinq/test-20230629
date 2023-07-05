f8aadc8c14ac69395e2d9ee82334cb8d0be8561f
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-07-05 15:27:18 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -1,5 +1,25 @@
 import streamlit as st
 from llm_processing import process_llm_request
+from Bio import Entrez, SeqIO
+from collections import Counter
+
+def count_codons(gene_ids, email):
+    Entrez.email = email  # Always tell NCBI who you are
+    codon_counts = {}
+
+    for gene_id in gene_ids:
+        handle = Entrez.efetch(db="nucleotide", id=gene_id, rettype="gb", retmode="text")
+        record = SeqIO.read(handle, "genbank")
+        handle.close()
+
+        sequence = str(record.seq)
+        codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)]  # split sequence into codons
+
+        # count codons
+        codon_count = Counter(codons)
+        codon_counts[gene_id] = codon_count
+
+    return codon_counts
 
 def main():
     st.title('Ovarian Cancer Diagnosis Interface')
@@ -24,10 +44,13 @@ def main():
     # Text input for symptoms
     symptoms = st.text_input('Enter Symptoms')
 
+    # Text input for gene IDs
+    gene_ids = st.text_input('Enter Gene IDs (comma-separated)')
+
     # Text input for doctor's question
     doctor_question = st.text_input('Enter your question for the Langchain LLM model')
 
-    if st.button('Submit'):
+    if st.button('Submit Patient Information'):
         st.write('Patient Information:')
         st.write('Name: ', patient_name)
         st.write('Age: ', patient_age)
@@ -43,6 +66,13 @@ def main():
         # Process the Langchain LLM model request
         result = process_llm_request(doctor_question, patient_name, patient_age, patient_bmi, patient_health, symptoms)
         st.write('LLM Result: ', result)
+
+    if st.button('Submit Gene IDs'):
+        # Split the gene IDs string into a list
+        gene_ids_list = [gene_id.strip() for gene_id in gene_ids.split(',')]
+        # Count codons for each gene
+        codon_counts = count_codons(gene_ids_list, "your_email@example.com")  # replace with your email
+        st.write('Codon Counts: ', codon_counts)
 
 if __name__ == "__main__":
     main()
