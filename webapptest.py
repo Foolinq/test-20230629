5178fbcd2355ccaec08c236d9f2739a8d29ebdd2
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-07-17 16:47:32 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -26,7 +26,7 @@ class Transcription(Base):
     gene = relationship('Gene', back_populates='transcriptions')
 
 # Set up the database
-engine = create_engine('postgresql://user:password@localhost/dbname')
+engine = create_engine('postgres://xmybdbirwwaocp:d2a20d22a09e4948cda6b9688769effa0a9f0b0393c0af7ba95d04b07b5d6fff@ec2-52-205-45-222.compute-1.amazonaws.com:5432/d9addgatbiak5p')
 Session = sessionmaker(bind=engine)
 
 # Create all tables if they don't exist
