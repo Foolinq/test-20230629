06820f899b708bbfc0aca238f97a89051ca1e554
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-07-27 11:51:10 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, String, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 # Get your database connection from the environment variable
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('DBURL')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
