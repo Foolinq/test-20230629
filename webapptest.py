4093c1087e625f5e5a2fa2db37150e83a5fa7201
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-07-27 10:57:23 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -1,98 +1,55 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, UniqueConstraint
+import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
-from collections import Counter
 
-# Define the database
-DATABASE_URI = 'postgresql://jqczrvezvwlzer:5980122424475b4fcdc2e539dbb0667d254452a21cd3cc633f9a64a8796da2df@ec2-3-212-70-5.compute-1.amazonaws.com:5432/d202roftknash2'
-engine = create_engine(DATABASE_URI)
-Session = sessionmaker(bind=engine)
+# Your database connection
+DATABASE_URL = "postgresql://jqczrvezvwlzer:5980122424475b4fcdc2e539dbb0667d254452a21cd3cc633f9a64a8796da2df@ec2-3-212-70-5.compute-1.amazonaws.com:5432/d202roftknash2"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Your Gene class
 class Gene(Base):
     __tablename__ = 'genes'
 
     name = Column(String, primary_key=True)
     sequence = Column(String)
 
-class Codon(Base):
-    __tablename__ = 'codons'
+Base.metadata.create_all(bind=engine)
 
-    id = Column(Integer, primary_key=True)
-    gene_name = Column(String, ForeignKey('genes.name'))
-    codon = Column(String)
-    amino_acid = Column(String)
-    count = Column(Integer)
-    __table_args__ = (UniqueConstraint('gene_name', 'codon', name='uc_gene_codon'), )
+def main():
+    st.title("CSV File Cleaning")
 
-# Define the genetic code
-GENETIC_CODE = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
-}
+    csv_file = st.file_uploader("Upload CSV file", type=['csv'])
+    if csv_file is not None:
+        df = pd.read_csv(csv_file)
 
-def setup_tables():
-    Base.metadata.create_all(engine)
+        if st.button("Start Cleaning"):
+            cleaned_df = clean_csv(df)
+            st.dataframe(cleaned_df)
+            csv_download_link(cleaned_df)
 
-def add_codon(gene_name, codon, amino_acid, count):
-    session = Session()
+def clean_csv(df):
+    # Create a session
+    session = SessionLocal()
 
-    try:
-        new_codon = Codon(gene_name=gene_name, codon=codon, amino_acid=amino_acid, count=count)
-        session.add(new_codon)
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+    # Get column names from 'genes' table in the database
+    table_columns = [c.name for c in Gene.__table__.columns]
+    
+    # Drop columns from dataframe that do not exist in the database table
+    df = df.loc[:, df.columns.isin(table_columns)]
+    
+    return df
 
-def process_gene_codons(gene_name):
-    session = Session()
-
-    try:
-        gene = session.query(Gene).filter_by(name=gene_name).first()
-
-        sequence = gene.sequence
-        codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)]
-
-        codon_counter = Counter(codons)
-
-        for codon, count in codon_counter.items():
-            if len(codon) != 3:
-                continue
-
-            amino_acid = GENETIC_CODE.get(codon, 'X')
-            add_codon(gene_name, codon, amino_acid, count)
-    except Exception as e:
-        print(f'Failed to process {gene_name}: {e}')
-    finally:
-        session.close()
+def csv_download_link(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode() 
+    href = f'<a href="data:file/csv;base64,{b64}" download="cleaned.csv">Download cleaned CSV File</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
 if __name__ == "__main__":
-    setup_tables()
-
-    # Query all the genes from your database
-    session = Session()
-    gene_names = [gene.name for gene in session.query(Gene).all()]
-    session.close()
-
-    # Process all the genes
-    for gene_name in gene_names:
-        process_gene_codons(gene_name)
-
-    print("Done!")
+    main()
