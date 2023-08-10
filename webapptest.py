853d54400e9892bb8ef84156c22687ebce42d2c3
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 15:24:21 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -1,32 +1,26 @@
 import os
-from langchain import OpenAI, SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
+from langchain import OpenAI, SQLDatabase
 import streamlit as st
 
-
-
+# Retrieve Heroku Config Vars
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 DBURL = os.environ.get('DBURL')
 
-
-
-
+# Set Up Database Connection
 db = SQLDatabase.from_uri(DBURL)
 
-
-
-
+# Set Up LangChain
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
 db_chain = SQLDatabaseChain.from_llm(llm, db)
 
-
-
-
+# Query Function
 def query_database(prompt):
-    return db_chain.call({"question": prompt})
+    inputs = {"question": prompt}
+    response = db_chain(inputs)
+    return response
 
-
-
+# Streamlit Interface
 def main():
     st.title('Natural Language Query Interface')
     user_input = st.text_input('Please enter your natural language query:')
