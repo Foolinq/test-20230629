7d75ec150c646380e5269a0646f7a3f890b06abc
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 18:16:20 -0400

    Verification test

--- webapptest.py
+++ webapptest.py
@@ -3,7 +3,6 @@ from langchain_experimental.sql import SQLDatabaseChain
 from langchain import OpenAI, SQLDatabase
 import streamlit as st
 
-
 # Retrieve Heroku Config Vars
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 DBURL = os.environ.get('DBURL')
@@ -18,15 +17,25 @@ db_chain = SQLDatabaseChain.from_llm(llm, db)
 # Query Function
 def query_database(prompt):
     inputs = {"query": prompt}
-    response = db_chain(inputs)
+    response = db_chain(inputs, return_sql=True)  # Return SQL without executing
     return response['result']
 
 # Streamlit Interface
 def main():
     st.title('Natural Language Query Interface')
     user_input = st.text_input('Please enter your natural language query:')
-    if user_input:
-        response = query_database(user_input)
-        st.write(response)
+    send_button = st.button('Send')  # Send button
+
+    if send_button and user_input:
+        sql_query = query_database(user_input)
+        st.write(f"The SQL query to be run is: {sql_query}")
+        confirm_button = st.button('Confirm')  # Confirm button
+
+        if confirm_button:
+            # Execute the SQL query
+            inputs = {"query": user_input}
+            response = db_chain(inputs)
+            st.write(response['result'])
+
 if __name__ == '__main__':
     main()
