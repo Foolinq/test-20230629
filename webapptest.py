7229858f7828f72614343a136578079978f518a9
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 15:16:41 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -17,20 +17,14 @@ db = SQLDatabase.from_uri(DBURL)
 
 
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
+db_chain = SQLDatabaseChain.from_llm(llm, db)
 
-PROMPT = """
-Given an input question, first create a syntactically correct postgresql query to run,
-then look at the results of the query and return the answer.
-The question: {question}
-"""
-
-db_chain = SQLDatabaseSequentialChain(llm=llm, database=db, verbose=True, top_k=3)
 
 
 
 def query_database(prompt):
-    question = QUERY.format(question=prompt)
-    return db_chain.run(question)
+    return db_chain.call({"question": prompt})
+
 
 
 def main():
