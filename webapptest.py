0d8a047be2b79062d879553f2657421951cdc0de
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 16:51:03 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -4,6 +4,17 @@ from langchain import OpenAI, SQLDatabase
 from langchain.schema.prompt_template import BasePromptTemplate
 import streamlit as st
 
+# Custom Prompt Class
+class CustomPrompt(BasePromptTemplate):
+    template: str = "This data represents genetic information. The 'codon_incorporation_rate' table contains the incorporation rate of each codon for the genes contained in the 'genes' for 578 patients. The 'codons' table contains the quantity of each codon for a couple hundred genes. The 'patient_gene_expression' table contains the level of expression of each gene in the 'genes' table for each patient. Query: {query}"
+    input_variables: list = ["query"]
+
+    def format(self, **kwargs):
+        return self.template.format(**kwargs)
+
+    def format_prompt(self, **kwargs):
+        return self.format(**kwargs)
+
 # Retrieve Heroku Config Vars
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 DBURL = os.environ.get('DBURL')
@@ -14,21 +25,10 @@ db = SQLDatabase.from_uri(DBURL)
 # Set Up LangChain
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
 
-class CustomPrompt(BasePromptTemplate):
-    template: str = (
-        "This data represents genetic information. The 'codon_incorporation_rate' table contains the incorporation rate of each codon for the genes contained in the 'genes' for 578 patients. The 'codons' table contains the quantity of each codon for a couple hundred genes. The 'patient_gene_expression' table contains the level of expression of each gene in the 'genes' table for each patient. Query: {query}"
-    )
-    input_variables: list = ["query"]
-
-    def format(self, **kwargs):
-        return self.template.format(**kwargs)
-
-    def format_prompt(self, **kwargs):
-        return self.format(**kwargs)
-
+# Create Custom Prompt Instance
+custom_prompt = CustomPrompt()
 
 # Set up LangChain with the custom prompt
-custom_prompt = CustomPrompt()
 db_chain = SQLDatabaseChain.from_llm(llm, db, prompt=custom_prompt)
 
 # Query Function
