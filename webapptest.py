9033d3985444147c7668cb9f6a37c39d7ec81246
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 17:57:54 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -1,19 +1,8 @@
 import os
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain import OpenAI, SQLDatabase
-from langchain.schema.prompt_template import BasePromptTemplate
 import streamlit as st
 
-# Custom Prompt Class
-class CustomPrompt(BasePromptTemplate):
-    template: str = "This data represents genetic information. The 'codon_incorporation_rate' table contains the incorporation rate of each codon for the genes contained in the 'genes' for 578 patients. The 'codons' table contains the quantity of each codon for a couple hundred genes. The 'patient_gene_expression' table contains the level of expression of each gene in the 'genes' table for each patient. Query: {query}"
-    input_variables: list = ["query"]
-
-    def format(self, **kwargs):
-        return self.template.format(**kwargs)
-
-    def format_prompt(self, **kwargs):
-        return self.format(**kwargs)
 
 # Retrieve Heroku Config Vars
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
@@ -24,17 +13,11 @@ db = SQLDatabase.from_uri(DBURL)
 
 # Set Up LangChain
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
-
-# Create Custom Prompt Instance
-custom_prompt = CustomPrompt()
-
-# Set up LangChain with the custom prompt
-db_chain = SQLDatabaseChain.from_llm(llm, db, prompt=custom_prompt)
+db_chain = SQLDatabaseChain.from_llm(llm, db)
 
 # Query Function
 def query_database(prompt):
     inputs = {"query": prompt}
-    response = db_chain(inputs=inputs)  # Pass the inputs dictionary with the key 'inputs'
     return response['result']
 
 # Streamlit Interface
@@ -44,6 +27,5 @@ def main():
     if user_input:
         response = query_database(user_input)
         st.write(response)
-
 if __name__ == '__main__':
     main()
