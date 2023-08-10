d3a8d49f52bc57b13da65f1b738fe30bf58036c0
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 18:20:36 -0400

    Bruh

--- webapptest.py
+++ webapptest.py
@@ -12,30 +12,27 @@ db = SQLDatabase.from_uri(DBURL)
 
 # Set Up LangChain
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
-db_chain = SQLDatabaseChain.from_llm(llm, db)
+db_chain = SQLDatabaseChain.from_llm(llm, db, return_sql=True)  # Set return_sql to True
 
 # Query Function
 def query_database(prompt):
     inputs = {"query": prompt}
-    response = db_chain(inputs, return_sql=True)  # Return SQL without executing
+    response = db_chain(inputs)  # Return SQL without executing
     return response['result']
 
 # Streamlit Interface
 def main():
     st.title('Natural Language Query Interface')
     user_input = st.text_input('Please enter your natural language query:')
-    send_button = st.button('Send')  # Send button
-
+    send_button = st.button('Send')
     if send_button and user_input:
         sql_query = query_database(user_input)
-        st.write(f"The SQL query to be run is: {sql_query}")
-        confirm_button = st.button('Confirm')  # Confirm button
-
+        st.write(f"SQL Query: {sql_query}")
+        confirm_button = st.button('Confirm')
         if confirm_button:
-            # Execute the SQL query
-            inputs = {"query": user_input}
-            response = db_chain(inputs)
-            st.write(response['result'])
+            db_chain.return_sql = False  # Set return_sql to False to execute the query
+            response = query_database(user_input)
+            st.write(response)
 
 if __name__ == '__main__':
     main()
