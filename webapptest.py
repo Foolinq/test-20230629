a5ac7eff17c8debbfabfdaddc5b63f0c5942c2bc
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 16:26:11 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -14,14 +14,17 @@ db = SQLDatabase.from_uri(DBURL)
 # Set Up LangChain
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
 
+
+
 # Define your custom prompt
-custom_prompt = PromptTemplate(
-    template="This data represents genetic information. The 'codon_incorporation_rate' table contains the incorporation rate of each codon for the genes contained in the 'genes' for 578 patients. The 'codons' table contains the quantity of each codon for a couple hundred genes. The 'patient_gene_expression' table contains the level of expression of each gene in the 'genes' table for each patient."
-    input_variables: ["query"]  # Define the input variables used in the prompt
-)
+custom_prompt = {
+    "template": "This data represents genetic information. The 'codon_incorporation_rate' table contains the incorporation rate of each codon for the genes contained in the 'genes' for 578 patients. The 'codons' table contains the quantity of each codon for a couple hundred genes. The 'patient_gene_expression' table contains the level of expression of each gene in the 'genes' table for each patient.",
+    "input_variables": ["query"]  # Define the input variables used in the prompt
+}
 
 # Set up LangChain with the custom prompt
 db_chain = SQLDatabaseChain.from_llm(llm, db, prompt=custom_prompt)
+
 
 # Query Function
 def query_database(prompt):
