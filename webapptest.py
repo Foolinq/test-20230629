6fb2e65dabd9b8617131fd721c7c6ec9832eb85a
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 14:35:58 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -1,39 +1,37 @@
 import os
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.sql_database import SQLDatabase
-from langchain.llms.openai import OpenAI
-from langchain.agents import AgentExecutor
-from langchain.agents.agent_types import AgentType
+from langchain import SQLDatabase, SQLDatabaseChain
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 
 
-
-db_url = os.environ.get("DBURL")
-db = SQLDatabase.from_uri(db_url)
-toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
+API_KEY = os.environ.get('OPENAI_API_KEY')
+DBURL = os.environ.get('DBURL')
 
 
+db = SQLDatabase.from_uri(DBURL)
 
 
-agent_executor = create_sql_agent(
-    llm=OpenAI(temperature=0),
-    toolkit=toolkit,
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-)
+llm = ChatOpenAI(temperature=0, openai_api_key=API_KEY, model_name='gpt-3.5-turbo')
+
+QUERY = """
+Given an input question, first create a syntactically correct postgresql query to run, then look at the results of the query and return the answer.
+Use the following format:
+
+Question: "Question here"
+SQLQuery: "SQL Query to run"
+SQLResult: "Result of the SQLQuery"
+Answer: "Final answer here"
+
+{question}
+"""
+
+db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
 
 
+def query_database(prompt):
+    question = QUERY.format(question=prompt)
+    return db_chain.run(question)
 
-
-def query_database(query):
-    result = agent_executor.run(query)
-    return result
-
-
-
-import streamlit as st
 
 def main():
     st.title('Natural Language Query Interface')
