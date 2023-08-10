a6dd267cd0e1fb2cc8b02810c8b7d7018088c234
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-10 14:18:13 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -1,39 +1,41 @@
-import os
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+from langchain.llms.openai import OpenAI
+from langchain.agents import AgentExecutor
+from langchain.agents.agent_types import AgentType
+from langchain.chat_models import ChatOpenAI
+
+
+
+db = SQLDatabase.from_uri("sqlite:///path/to/your/database.db")
+toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
+
+
+
+agent_executor = create_sql_agent(
+    llm=OpenAI(temperature=0),
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
+
+
+
+def query_database(query):
+    result = agent_executor.run(query)
+    return result
+
+
+
 import streamlit as st
-import psycopg2
-import openai  # OpenAI's Python client library
 
-# Function to translate natural language prompt to SQL using GPT (via LangChain)
-def translate_prompt_to_sql(prompt):
-    # TODO: Implement the actual call to LangChain or GPT to translate the prompt to SQL
-    # For now, returning a mock SQL command
-    return f"SELECT * FROM my_table WHERE my_column = '{prompt}';"
+def main():
+    st.title('Natural Language Query Interface')
+    user_input = st.text_input('Please enter your natural language query:')
+    if user_input:
+        response = query_database(user_input)
+        st.write(response)
 
-# Streamlit interface
-st.title("Natural Language to SQL Translator")
-
-# User input for natural language prompt
-user_prompt = st.text_input("Enter your natural language prompt:")
-
-# Translate the prompt to SQL using GPT (via LangChain)
-sql_command = translate_prompt_to_sql(user_prompt)
-
-# Display the generated SQL for user confirmation
-if user_prompt:
-# TODO: Make generated SQL visible before user confirmation
-    if st.button("Execute the following SQL command?"):
-        st.text_area("Generated SQL:", value=sql_command)
-
-        # Connect to the database and execute the SQL command
-        CONNECTION_STRING = os.environ['DBURL']
-        conn = psycopg2.connect(CONNECTION_STRING)
-        cursor = conn.cursor()
-        cursor.execute(sql_command)
-        results = cursor.fetchall()
-        conn.close()
-
-        # Display the results
-        st.write(results)
-
-# Placeholder for future features
-st.write("More features coming soon!")
+if __name__ == '__main__':
+    main()
