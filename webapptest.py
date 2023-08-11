984f45aa8f8f07085cde532506535234c3a01e6c
Foolinq <128741216+Foolinq@users.noreply.github.com>
2023-08-11 14:31:18 -0400

    Update webapptest.py

--- webapptest.py
+++ webapptest.py
@@ -14,12 +14,20 @@ db = SQLDatabase.from_uri(DBURL)
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
 db_chain = SQLDatabaseChain.from_llm(llm, db)
 
-# Query Function
 def query_database(prompt, return_sql=False):
+    # Preprocess the prompt
+    processed_prompt = preprocess_input(prompt)
+    
     db_chain.return_sql = return_sql
-    inputs = {"query": prompt}
+    inputs = {"query": processed_prompt}
     response = db_chain(inputs)
     return response['result']
+
+def preprocess_input(user_input):
+    # Add context or researched information to the user input
+    context = "The data is housed in a PostgreSQL database and consists of gene-related information. There are tables containing gene annotations, detailing the characteristics and functions of genes. Other tables count the codons within coding sequences, providing insights into genetic coding structure. Additionally, there are tables with specific information related to ovarian cancer patients, including gene expression levels and codon incorporation rates. Together, these tables offer a multifaceted view of genetic data and its relevance to ovarian cancer."
+    researched_info = ""
+    return f"{context} {researched_info} {user_input}"
 
 # Streamlit Interface
 def main():
